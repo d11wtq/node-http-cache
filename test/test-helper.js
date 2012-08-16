65acfff3a11e11903b6382713d39e2b00af7cbe4
d11wtq <chris@w3style.co.uk>
2012-08-17 03:10:43 +1000

    Copyright notice

--- test/test-helper.js
+++ test/test-helper.js
@@ -1,5 +1,9 @@
-/**
- * Convenience stuff for testing.
+/*
+ * node-http-cache.
+ * Copyright © 2012 Chris Corbyn.
+ *
+ * See LICENSE file for details.
  */
 
+/* Convenience stuff for testing */
 var httpCache = exports.httpCache = require('../lib/http-cache');
