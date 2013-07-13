e01cbd2abcbd98b53a62e182e0d609c0fdef1b97
motemen <motemen@gmail.com>
2013-07-14 00:48:27 +0900

    Use appropriate cache flags

--- test/http-cache/storage/memory-test.js
+++ test/http-cache/storage/memory-test.js
@@ -8,6 +8,7 @@
 var helper    = require('../../test-helper')
   , shared    = require('../../support/shared-examples')
   , httpCache = helper.httpCache
+  , assert    = require('assert')
   , memo      = helper.memo
   ;
 
@@ -17,4 +18,24 @@ describe('httpCache.storage.MemoryStorage', function(){
   });
 
   shared.behavesLikeACacheStorage(storage);
+
+  var method  = memo().is(function(){ return 'GET' });
+  var headers = memo().is(function(){ return {} });
+
+  var req = memo().is(function(){
+    return helper.createRequest({
+      method:  method(),
+      headers: headers()
+    });
+  });
+
+  var res = memo().is(function(){
+    return helper.createResponse(req());
+  });
+
+  it('tries to serve cache if cacheable', function(){
+    var serveCachedSpy = helper.sinon.spy(storage(), 'serveCached');
+    storage().handleRequest(req(), res());
+    assert(serveCachedSpy.called);
+  });
 });
